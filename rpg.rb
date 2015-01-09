3b2fed211aa36339a129bdc98b5ac40d7c63aa47
katrina gardner <yutzeng0314@gmail.com>
2015-01-09 16:56:38 -0500

    added fight

--- rpg.rb
+++ rpg.rb
@@ -2,12 +2,12 @@ puts "Hello and welcome to Onslaughter!"
 puts "Please say your name:"
 name = gets.chomp
 student = {
-	"name" => name, 
+	"name" => name,
 	"willpower" => 10,
 	"damage" => 4,
 	"hit_chance" => 95,
 	"run_chance" => 90,
-	"enemies_defeated"=> 0, 
+	"enemies_defeated"=> 0,
 }
 ruby = {
 	"name" => "Ruby",
@@ -32,39 +32,54 @@ css = {
 }
 enemies = [ruby, js, css]
 
-def fight()
-	p "Fighting"
+def fight(curr,student)
+	p "fight"
+  while (curr["willpower"] > 0 && student["willpower"] > 0)
+    p "Attack"
+    hit = Random.rand(1...100)
+    if hit > student["hit_chance"]
+      p "You missed"
+    else
+      p "You hit for #{student["damage"]}"
+      curr["willpower"] -= student["damage"]
+    end
+    p curr["willpower"]
+    if curr["willpower"] > 0
+          roll =  Random.rand(1...100)
+      if roll < curr["hit_chance"]
+        p "Hit you with #{curr["attack"]} and does #{curr["damage"]}"
+        student["willpower"] -= curr["damage"]
+      else
+        p "code missed"
+      end
+   end
 end
-
+end
 puts "Hi #{name}! Let's start coding!"
 puts "You have a willpower rating of 10, lets see how you fair
 against GA course material"
 
-ran_en = Random.rand(0...enemies.length)
-current = enemies[ran_en]
-p "You encouneter a wild #{current["name"]}"
-corr_ans = true
-while corr_ans
-	p "Do you want to run? (y/n)"
-	answer = gets.chomp
-	if(answer == "y")
-		corr_ans = false
-		student["willpower"] -= 2
-		p "You now have #{student["willpower"]} health left."
-		run = Random.rand(1...100)
-		if run > student["run_chance"]
-			p "Could not run, you need to code"
-			fight()
-		end
-	elsif(answer == "n")
-		corr_ans = false
-		fight()
-	end
+while student["willpower"] > 0
+  ran_en = Random.rand(0...enemies.length)
+  current = enemies[ran_en]
+  p "You encouneter a wild #{current["name"]}"
+  corr_ans = true
+  while corr_ans
+  	p "Do you want to run? (y/n)"
+  	answer = gets.chomp
+  	if(answer == "y")
+  		corr_ans = false
+  		student["willpower"] -= 2
+  		p "You now have #{student["willpower"]} health left."
+  		run = Random.rand(1...100)
+  		if run > student["run_chance"]
+  			p "Could not run, you need to code"
+  			fight(current)
+  		end
+
+  	elsif(answer == "n")
+  		corr_ans = false
+  		fight(current,student)
+  	end
+  end
 end
-
-
-
-
-# while student["willpower"] > 0
-
-# end
